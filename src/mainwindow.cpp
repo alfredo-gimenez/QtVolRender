91f0cde1a01e0268197b13b7e2ff457cd0b280d5
Alfredo Gimenez <alfredo.gimenez@gmail.com>
2015-04-20 16:15:15 -0700

    strip mainwindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,10 +10,6 @@ using namespace std;
 
 #include "scatterplotviz.h"
 
-// Correlation matrix based on selection
-// 3D scatterplot using cubes as elements
-// Reset parallel coordinates spacing
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -31,24 +27,11 @@ MainWindow::MainWindow(QWidget *parent) :
 
     // Add to viz widgets
     vizWidgets.push_back(scatterPlotViz);
-
-    //for(int i=0; i<vizWidgets.size(); i++)
-    //{
-    //    connect(vizWidgets[i], SIGNAL(repaintAll()), this, SLOT(repaintAllVizWidgets()));
-    //}
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::repaintAllVizWidgets()
-{
-    for(int i=0; i<vizWidgets.size(); i++)
-    {
-        vizWidgets[i]->repaint();
-    }
 }
 
 int MainWindow::importData()
@@ -58,67 +41,5 @@ int MainWindow::importData()
                                                      "",
                                                      tr("Files (*.*)"));
 
-    DataObject *dobj = parseCSVFile(dataFileName);
-    if(dobj == NULL)
-        return -1;
-
-    dataObjects.push_back(dobj);
-
-    for(int i=0; i<vizWidgets.size(); i++)
-        vizWidgets[i]->setData(dobj);
-
-    repaintAllVizWidgets();
-
     return 0;
 }
-
-DataObject* MainWindow::parseCSVFile(QString dataFileName)
-{
-    DataObject *dobj = new DataObject();
-
-    // Open the file
-    QFile dataFile(dataFileName);
-
-    if (!dataFile.open(QIODevice::ReadOnly | QIODevice::Text))
-         return NULL;
-
-    // Create text stream
-    QTextStream dataStream(&dataFile);
-    QString line;
-    QStringList lineValues;
-    long long elemid = 0;
-
-    // Get metadata from first line
-    line = dataStream.readLine();
-    dobj->meta = line.split(',');
-    dobj->numDimensions = dobj->meta.size();
-
-    // Get data
-    while(!dataStream.atEnd())
-    {
-        line = dataStream.readLine();
-        lineValues = line.split(',');
-
-        if(lineValues.size() != dobj->numDimensions)
-        {
-            cerr << "ERROR: element dimensions do not match metadata!" << endl;
-            cerr << "At element " << elemid << endl;
-            return NULL;
-        }
-
-        for(int i=0; i<lineValues.size(); i++)
-        {
-            dobj->vals.push_back(lineValues[i].toDouble());
-        }
-
-        elemid++;
-    }
-
-    // Close and return
-    dataFile.close();
-
-    dobj->init();
-
-    return dobj;
-}
-
